e01a78e94d3e8873fe221ba21346966ff271ba40
jomorlier <joseph.morlier@isae.fr>
2019-02-15 16:38:28 +0100

    Add files via upload

---
+++ MDA/y12.m
@@ -0,0 +1,7 @@
+% This m-file is a user-defined function used to implement the fixed point
+% iteration example in the CNS 3320 Matlab tutorial.
+%
+% Author: James Allison
+% Date: 1/04/05
+function y = y12(x)
+y = 2+2*x;
---
+++ MDA/y21.m
@@ -0,0 +1,7 @@
+% This m-file is a user-defined function used to implement the fixed point
+% iteration example in the CNS 3320 Matlab tutorial.
+%
+% Author: James Allison
+% Date: 1/04/05
+function y = y21(x)
+y = 1/4*x-1/4;
